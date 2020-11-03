89127a9fb4a1f5156a66ecda1f5d11fc6de67a54
Vinayak <vinayak@celectengine.com>
2020-11-02 21:29:59 -0500

    added periodic trader

---
+++ implementation1/periodic_trader_vin.py
@@ -0,0 +1,83 @@
+# Import packages.
+import datetime
+import json
+import os
+import sys
+import time
+
+import pandas as pd
+import numpy as np
+import redis
+from pandas.core.common import SettingWithCopyWarning
+
+import warnings
+warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
+
+# CloudEx imports.
+import cloud_ex
+
+# Import AlgorithmicTrader helper class.
+from algorithmic_trader_shay import AlgorithmicTrader
+from algorithmic_trader_shay import summarize_historical_trades_df
+
+# MOMENTUM
+class PeriodicTraderStrategy(AlgorithmicTrader):
+    def __init__(self, trader, symbol_list, bin_interval_ms=500):
+        """
+        Momentum Trader Class.
+
+        :param trader(cloud_ex.Trader): CloudEx's base Trader object
+        :param symbol_list: List of ticker symbols (str) to fetch data for.
+        :param bin_interval_ms: Frequency with which to bin trading data in milliseconds (int).
+        """
+        # Initialize Trader.
+        AlgorithmicTrader.__init__(self, trader, symbol_list, bin_interval_ms=bin_interval_ms)
+
+    def algorithm(self, df, **kwargs):
+        """
+        Calculate buy sell points for an equity.
+        A buy is triggered when the weighted rate of change for the past two time intervals is above a given threshold.
+        A sell is triggered when the weighted rate of change for the past two time intervals is below a given threshold.
+        For this implementation we evaluate the rate of change for stock price from open to close for each bin_interval_ms time interval.
+
+        :param df: Dataframes with prices for an equity (pd.DataFrame).
+        Momentum specific arguments:
+        :param threshold: Weighted rate of change above/below which we will buy/sell (float).
+        :param p1: weight for t-1 rate of change, Note: p1+p2=1 (float).
+        :param p2: Weight for t-2 rate of change, Note: p1+p2=1 (float).
+
+        Returns: 
+            An (action, price) pair.
+            
+            'action' is None when no action is determined to be taken. This happens either when we do not have enough
+            data to build a moving average estimate or if the price is within a small threshold above/below
+            the moving average. In this case, 'price' is also None.
+
+            'action' is 'Buy' when the price rises above the threshold defined for the moving average. 
+                The buy price is set to the most recent closing price.
+                
+            'action' is 'Sell' when the price drops below the threshold defined for the moving average. 
+                The sell price is set to the most recent closing price.
+        """
+        # Handle case when data not available to get momentum.
+        if len(df) < 20:
+            return None, None
+        
+        y_timeseries = df['ClosePrice']
+        # smooth timeseries
+        box_pts, ma_length = 10, 10
+        box = np.ones(box_pts)/box_pts
+        y_smooth = np.convolve(y_timeseries, box, mode='same')
+        
+        dydx = np.diff(y_smooth)/np.diff(range(len(y_smooth)))
+        dydx = np.insert(dydx, 0, 0.0, axis=0)
+    
+        sign_dy_current = math.copysign(1, np.mean(dydx[-ma_length:]))
+        sign_dy_prev = math.copysign(1, np.mean(dydx[-2*ma_length: -ma_length]))
+
+        if sign_dy_current > sign_dy_prev:
+            'Buy', y_timeseries[-1]
+        elif sign_dy_current < sign_dy_prev:
+            'Sell', y_timeseries[-1]
+        else:
+            None, None
